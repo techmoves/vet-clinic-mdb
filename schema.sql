3ed07e19c716bd1c2121612c8f03cf5109b87a9a
techmoves <muhndlovu28@gmail.com>
2023-07-18 20:46:51 +0200

    Add queries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id int PRIMARY KEY NOT NULL,
+    name varchar(255),
+    date_of_birth: DATE
+    escape_attempts: int,
+    neutered: boolean,
+    weight_kg: decimal(10,2)
 );

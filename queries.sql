2059bf16acf1c462eb5cd09ed33d2c3b8d3a7bfc
techmoves <muhndlovu28@gmail.com>
2023-07-21 14:24:53 +0200

    Updated reame file and add license

--- queries.sql
+++ queries.sql
@@ -26,8 +26,71 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg  >= 10.4 AND weight_kg  <= 17.3;
 
     
-    
+   ------------------transacriotion--- 
    
+   -- Begin the transaction
+START TRANSACTION;
+
+-- Update the "species" column to "unspecified" for all rows in the "animals" table
+UPDATE animals SET species = '';
+
+-- Verify the changes by selecting all rows from the "animals" table
+SELECT * FROM animals;
+
+-- Roll back the transaction to revert the changes made in Step 2
+ROLLBACK;
+
+-- Verify that the "species" column has gone back to its state before the transaction
+SELECT * FROM animals;
+
+    -- INSIDE TRANSACTION
     
-    
-    
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+-------------------------------Verify and commit -----
+
+
+-- Delete all animals born after Jan 1st, 2022.
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+-- create a savepoint
+
+
+-- Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+ROLLBACK;
+
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
+
+------------counts--------
+-- How many animals are there?
+SELECT COUNT(*) AS total_animals FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) AS average_weight FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, SUM(escape_attempts) AS escape_count FROM animals GROUP BY neutered
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT neutered,
+       MIN(weight_kg) AS minimum_weight,
+       MAX(weight_kg) AS maximum_weight
+FROM animals
+GROUP BY neutered;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species,
+       AVG(escape_attempts) AS average_escape_attempts
+FROM animals
+WHERE date_of_birth  BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
